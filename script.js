954f50a4dc845325cccdaba21b83d29dbcc24ccd
g25nf <160293673+g25nf@users.noreply.github.com>
2024-07-07 09:17:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+//Transisi Awal View
+
+function fadeInElements() {
+  var fadeUpElements = document.querySelectorAll('[kezt-transition1], [kezt-transition2], [kezt-transition3], [kezt-transition4], [kezt-transition5]');
+  fadeUpElements.forEach(function(element) {
+    var rect = element.getBoundingClientRect();
+    var elementTop = rect.top + window.pageYOffset;
+    var windowHeight = window.innerHeight;
+    var scrollPosition = window.scrollY;
+    
+    if (elementTop < windowHeight + scrollPosition) {
+      if (!element.hasAttribute('kezt-transitioned')) {
+        if (element.hasAttribute('kezt-transition1')) {
+          element.style.opacity = 1;
+          element.style.top = 0;
+        } else if (element.hasAttribute('kezt-transition2')) {
+          element.style.opacity = 1;
+          element.style.left = 0;
+        } else if (element.hasAttribute('kezt-transition3')) {
+          element.style.opacity = 1;
+          element.style.right = 0;
+        } else if (element.hasAttribute('kezt-transition4')) {
+          element.style.opacity = 1;
+          element.style.bottom = 0;
+        } else if (element.hasAttribute('kezt-transition5')) {
+          element.style.opacity = 1;
+        }
+        element.setAttribute('kezt-transitioned', 'true');
+      }
+    }
+  });
+}
+
+window.addEventListener('scroll', function() {
+  fadeInElements();
+});
+
+window.onload = function() {
+  fadeInElements();
+};
+
+
+
+//Navigasi
+
+document.addEventListener("DOMContentLoaded", function() {
+    const openButton = document.getElementById("opennavigasi");
+    const closeButton = document.getElementById("closenavigasi");
+    const sidebar = document.getElementById("sidebarnav");
+    const menuheader = document.getElementById("menuheader");
+
+    openButton.addEventListener("click", function() {
+        sidebar.style.height = "100%";
+        openButton.style.bottom = "50px";
+    });
+
+    closeButton.addEventListener("click", function() {
+        sidebar.style.height = "0%";
+        openButton.style.bottom = "0px";
+    });
+});
